595124ba0724b0c4e89f8807e1ceee1e160fc0be
tan <bigmouth3033@gmail.com>
2023-11-15 10:57:58 +0700

    15/11 commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'users';
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Http\Controllers\AmenityController;
-use App\Http\Controllers\BookingController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\DistrictController;
-use App\Http\Controllers\PropertyAmenityController;
-use App\Http\Controllers\PropertyController;
-use App\Http\Controllers\PropertyImageController;
-use App\Http\Controllers\PropertyReviewController;
-use App\Http\Controllers\PropertyTypeController;
-use App\Http\Controllers\ProvinceController;
-use App\Http\Controllers\ProvincesController;
-use App\Http\Controllers\RoomTypeController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\Api\AuthController;
 use App\Models\PropertyType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,22 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::prefix('v1')->group(function(){
-    Route::get('booking',[BookingController::class,'getData']);
-    Route::get('categories',[CategoryController::class,'getData']);
-    Route::get('districts',[DistrictController::class,'getData']);
-    Route::get('amenities',[AmenityController::class,'getData']);
-    Route::get('properties',[PropertyController::class,'getData']);
-    Route::get('property_emenities',[PropertyAmenityController::class,'getData']);
-    Route::get('property_images',[PropertyImageController::class,'getData']);
-    Route::get('property_reviews',[PropertyReviewController::class,'getData']);
-    Route::get('property_type',[PropertyTypeController::class,'getData']);
-    Route::get('provinces',[ProvinceController::class,'getData']);
-    Route::get('room_type',[RoomTypeController::class,'getData']);
-    Route::get('transactions',[TransactionController::class,'getData']);
-    Route::get('users',[UserController::class,'getData']);
-});
+// Route::prefix('v1')->group(function(){
+//     Route::get('booking',[BookingController::class,'getData']);
+//     Route::get('categories',[CategoryController::class,'getData']);
+//     Route::get('districts',[DistrictController::class,'getData']);
+//     Route::get('amenities',[AmenityController::class,'getData']);
+//     Route::get('properties',[PropertyController::class,'getData']);
+//     Route::get('property_emenities',[PropertyAmenityController::class,'getData']);
+//     Route::get('property_images',[PropertyImageController::class,'getData']);
+//     Route::get('property_reviews',[PropertyReviewController::class,'getData']);
+//     Route::get('property_type',[PropertyTypeController::class,'getData']);
+//     Route::get('provinces',[ProvinceController::class,'getData']);
+//     Route::get('room_type',[RoomTypeController::class,'getData']);
+//     Route::get('transactions',[TransactionController::class,'getData']);
+//     Route::get('users',[UserController::class,'getData']);
+// });
+
+
+Route::get("/signup", [AuthController::class, 'signup']);
