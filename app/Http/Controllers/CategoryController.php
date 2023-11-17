c9e664a6b22d90b5f0b509fe1f735b49bf5dce9c
mainguyen <nguyenthingocmai193.com>
2023-11-17 17:13:09 +0700

    haiz

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,7 +8,36 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     //
-    function getData(){
-        return Category::all();
+    // function getData(){
+    //     return Category::all();
+    // }
+
+    public function create(Request $request)
+    {
+        // tạo biến instance từ model Category để truy vấn vào các cột của bảng
+        $Category = new Category;
+
+        $validatedData = $request->validate([
+            'name' => 'required|string| max:255',
+            'iconName' => 'required|max:200| mines: jpeg, png, jpg, gif, sgv',
+            'description' => 'required|string'
+        ]);
+
+        //check đầu vào và gán cho các thuộc tính của object Categpry
+
+        //cách 1: trực tiếp gán giá trị từ biến $request vào các thuộc tính của đối tượng $Category.
+        // $Category->name = $request->name;    
+        // $Category->icon = $request->iconName;  
+        // $Category->description = $request->description;
+
+        //bên trái: tên cột trong database, bên phải trùng key của validate
+        $Category->name = $validatedData['name'];
+        $Category->icon = $validatedData['iconName'];
+        $Category->description = $validatedData['description'];
+
+
+
+        $Category->save();
+        return response()->json([""]);
     }
 }
