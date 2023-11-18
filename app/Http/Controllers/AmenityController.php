5fd19f5c152ca11edc290784cd45413805c6e7f6
mainguyen <nguyenthingocmai193.com>
2023-11-18 18:58:12 +0700

    cham

--- app/Http/Controllers/AmenityController.php
+++ app/Http/Controllers/AmenityController.php
@@ -8,7 +8,24 @@ use Illuminate\Http\Request;
 class AmenityController extends Controller
 {
     //
-    function getData(){
-        return Amenity::all();
+    public function create(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:50',
+            'icon' => 'required|image|mimes:jpeg,png,jpg,gif,svg', // Adjust validation rules for the image
+        ]);
+        $Amenity = new Amenity;
+        $Amenity->name = $validatedData['name'];
+        //Lưu hình ảnh vào public
+        $imagePath = $request->file('icon')->store('public/images/amenties');
+        //Trích xuất tên tệp thực tế từ đường dẫn được lưu trữ
+        $fileNameImage = basename($imagePath);
+        $Amenity->icon_image = $fileNameImage;
+        $Amenity->save();
+        return response()->json([
+            "success" => true,
+            "message" => "Product created successfully.",
+            "data" => $Amenity
+        ]);
     }
 }
