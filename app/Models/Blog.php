f12776fa53c23005be4322537bfea887767934e8
mainguyen <nguyenthingocmai193.com>
2023-12-12 21:18:37 +0700

    sau khi them blog

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Blog extends Model
 {
     use HasFactory;
     protected $table = 'blogs';
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(BlogOfCate::class, 'id_blog', 'id_blog_categories');
+    }
 }
